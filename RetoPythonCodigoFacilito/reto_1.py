645b132d0f7c66afd6c6cdaa9adc611fa6c1836c
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-02-07 10:53:47 -0600

    Reto dos, agregar ids a los usuarios registrados

--- RetoPythonCodigoFacilito/reto_1.py
+++ RetoPythonCodigoFacilito/reto_1.py
@@ -1,3 +1,4 @@
+
 def userNew(name, last_name, email):
     return f"Hola {name} {last_name} en breve recibirás un correo a {email}"
 
@@ -11,7 +12,7 @@ def validates(field, len_min, len_max, name_field):
         return validates(field, len_min, len_max, name_field)
     
 
-def register(num_user):
+def register(num_user, ids):
     print(f"Ingresa los datos del Usuario {num_user}")
     name = validates(input("Ingresa su nombre: "), 5, 50, "nombre")
     last_name = validates(input(f"Ingresa su apellido: "), 5, 50, "apellido")
@@ -19,6 +20,7 @@ def register(num_user):
     email = validates(input("Ingresa su correo: "), 5, 50, "email")
 
     user = {
+        "id": ids,
         "name": name,
         "last_name": last_name,
         "number_phone": number_phone,
@@ -26,15 +28,45 @@ def register(num_user):
         }
     return user
 
+def idsAdd(num_user):
+    ids = num_user * 19981205
+    id_hex = ""
+    while ids > 0:
+        aux = ids % 16
+
+        if aux == 10:
+            id_hex += "A"
+        elif aux == 11:
+            id_hex += "B"
+        elif aux == 12:
+            id_hex += "C"
+        elif aux == 13:
+            id_hex += "D"
+        elif aux == 14:
+            id_hex += "E"
+        elif aux == 15:
+            id_hex += "F"
+        else:
+            id_hex += str(aux)
+
+        ids = ids // 16
+
+    return id_hex[::-1]
+
 
 def main():
     n = int(input("Cuantos usuarios deseas registrar: "))
     users = []
+    ids = []
 
     for i in range(n):
-        users.append(register(i+1))
+        ids.append(idsAdd(i+1))
+        users.append(register(i+1, ids[i]))
         print(f"El Usuario {i+1} ha sido registrado con exito!!!!!")
         print(f"En breve recibira un correo a {users[i]['email']}\n")
+
+    print("Los ids de los usuarios registrados son: ")
+    print(ids)
 
     """
     Mostrar los datos de todos los usuarios registrados
