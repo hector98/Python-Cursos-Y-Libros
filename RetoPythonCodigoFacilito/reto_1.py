b141711c88eda998dcab08957163a03a9bb2f098
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-02-08 16:45:27 -0600

    reto 4, agregar opcion de buscar y editar usuario

--- RetoPythonCodigoFacilito/reto_1.py
+++ RetoPythonCodigoFacilito/reto_1.py
@@ -1,4 +1,5 @@
-
+import os
+import time
 def userNew(name, last_name, email):
     return f"Hola {name} {last_name} en breve recibirás un correo a {email}"
 
@@ -29,7 +30,7 @@ def register(num_user, ids):
     return user
 
 def idsAdd(num_user):
-    ids = num_user * 19981205
+    ids = num_user * int(time.clock_gettime(1))
     id_hex = ""
     while ids > 0:
         aux = ids % 16
@@ -54,19 +55,77 @@ def idsAdd(num_user):
     return id_hex[::-1]
 
 
+def editUser():
+    print("Ingresa los datos del usuario")
+    name = validates(input("Ingresa su nombre: "), 5, 50, "nombre")
+    last_name = validates(input(f"Ingresa su apellido: "), 5, 50, "apellido")
+    number_phone = validates(input("Ingresa su numero de telefono: "), 10, 10, "numero de telefono")
+    email = validates(input("Ingresa su correo: "), 5, 50, "email")
+
+    edit_user = {
+        "name": name,
+        "last_name": last_name,
+        "number_phone": number_phone,
+        "email": email
+        }
+    return edit_user
+
+def findUserById(users, id_u):
+    find = False
+    find_user = ""
+    for user in users:
+        if user["id"] == id_u:
+            find = True
+            find_user = user
+            break
+    if find:
+        print(f"\nLos datos del usuario son: \nId = {find_user['id']}\nName = {find_user['name']}\nLast Name = {find_user['last_name']}\nNumber Phone = {find_user['number_phone']}\nEmail = {find_user['email']}\n")
+
+        print("Deseas editar el usuario? Si(s) No(n)")
+        op = input("Ingresa tu opcion: ")
+        if op.lower() == "s":
+            edit_user = editUser()
+            find_user['name'] = edit_user["name"]
+            find_user['last_name'] = edit_user["last_name"]
+            find_user['number_phone'] = edit_user["number_phone"]
+            find_user['email'] = edit_user["email"]
+            print("Usuario editado con exito")
+    else:
+        return "No se encontro usuario con ese id"
+
+
+def menu(users = [], ids = []):
+    #os.system("clear")
+    print("Menu: \n(1)Deseas registrar nuevos usuarios? \n(2)Deseas mostrar todos los ides de los usuarios? \n(3)Deseas mostrar los datos de un usuario por su id?")
+    option = int(input("Ingresa tu opcion: "))
+    
+    if option == 1:
+        n = int(input("Cuantos usuarios deseas registrar: "))
+        for i in range(n):
+            ids.append(idsAdd(i+1))
+            users.append(register(i+1, ids[i]))
+            print(f"El Usuario {i+1} ha sido registrado con exito!!!!!")
+            print(f"En breve recibira un correo a {users[i]['email']}\n")
+    elif option == 2:
+        for id in ids:
+            print(id)
+    elif option == 3:
+        id = input("Ingresa el id del usuario: ")
+        findUserById(users, id)
+    else:
+        print("Ingresa una opcion valida")
+        return menu(users, ids)
+
+    print("Deseas volver al menu?")
+    continue_ = input("Ingresa 'S' o 'N': ")
+    return menu(users, ids) if continue_.upper() == "S" else exit
+
+
 def main():
-    n = int(input("Cuantos usuarios deseas registrar: "))
     users = []
     ids = []
-
-    for i in range(n):
-        ids.append(idsAdd(i+1))
-        users.append(register(i+1, ids[i]))
-        print(f"El Usuario {i+1} ha sido registrado con exito!!!!!")
-        print(f"En breve recibira un correo a {users[i]['email']}\n")
-
-    print("Los ids de los usuarios registrados son: ")
-    print(ids)
+    menu(users, ids)
+    print("Gracias por usar el programa")
 
     """
     Mostrar los datos de todos los usuarios registrados
